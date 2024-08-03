2c658d035c5d80ad6031a1f943e1df11f799954b
kntbemad <102579507+kntbemad@users.noreply.github.com>
2024-08-03 03:02:04 -0400

    lots of changes

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,29 @@
 export function addTodoDOM(todo){
     const todoList = document.querySelector(".todoList");
-    let todoText  = document.createElement("p");
+    let newTodo = document.createElement("div");
+    let todoText  = document.createElement("label");
+    let todoCheck = document.createElement("input");
+
+    todoCheck.type = "checkbox";
+    todoCheck.id = todo.name + "cbox";
+    todo.name = todo.name;
+    todoText.for = todo.name;
     todoText.textContent = todo.name;
-    todoList.appendChild(todoText);
+   
+    newTodo.id = todo.name + "div";
+    newTodo.classList.add("todoEntry");
+
+    newTodo.appendChild(todoCheck);
+    newTodo.appendChild(todoText); 
+    todoList.appendChild(newTodo);
 }
 
 export function showForm(){
-    document.querySelector("#addform").style.display = "block";
+    document.querySelector("#addformdiv").style.display = "block";
+}
+
+
+export function closeForm(){
+    document.getElementById("addform").reset();
+    document.querySelector("#addformdiv").style.display = "none";
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import {addTodoDOM, showForm} from "./dom.js";
+import {addTodoDOM, showForm, closeForm} from "./dom.js";
 import './style.css';
+
 
 class todoList {
     constructor(name){
@@ -7,31 +8,64 @@ class todoList {
         this.name = name;
     }
 
-    addEntry(entry){
-        if(entry instanceof todoEntry){
-            this.list.push()
-            addTodoDOM(entry);
-        }
+    addEntry(name, desc){
+        let newTodo = new todoEntry(name, desc);
+        this.list.push();
+        addTodoDOM(newTodo);
     }
 }
 
 class todoEntry {
-    constructor(name){
+    constructor(name, desc){
         this.name = name;
+        this.desc = desc;
         this.active = true;
     }
 }
 
+function addTodo(name, desc){
+    let listName = document.querySelector(".todoList").id;
+    let currlist = listOfLists.find(e => e.name === listName);
+    currlist.addEntry(name, desc);
+
+    let todoentrydiv = document.getElementById(name + "div");
+    let cbox = document.getElementById(name + "cbox");
+    cbox.addEventListener("click", e => {
+        todoentrydiv.style.transition = "opacity 1000ms ease";
+        todoentrydiv.style.opacity = 0;
+        setTimeout(function(){
+            todoentrydiv.parentNode.removeChild(todoentrydiv);
+        }, 900);
+    }); 
+}
+
+function addButtons(){
+    let addformbtn = document.querySelector("#addtodo");
+    addformbtn.addEventListener("click", e => {
+        console.log("what");
+        showForm();
+    });
+
+    let submitbtn = document.querySelector("#submitform");
+    submitbtn.addEventListener("click", e =>{
+        let nameformtext = document.getElementById("name").value;
+        let descformtext = document.getElementById("desc").value;
+        addTodo(nameformtext, descformtext);
+        console.log("help");
+        e.preventDefault();
+        closeForm();
+    });
+}
+
+let listOfLists = [];
 let mainList = new todoList("main");
+listOfLists.push(mainList);
+addTodo("change layout, sidebar, main chunk inside this rounded div", "wwahahwa");
+addTodo("add details, date, priority, button to view for each task", "wahahsd");
+addTodo("ability to add new lists, swap in side bar", "please");
+addTodo("finish this todo app", "please");
 
-let addformbtn = document.querySelector("#addtodo");
-addformbtn.addEventListener("click", e => {
-    showForm();
-});
+addButtons();
 
-
-mainList.addEntry(new todoEntry("yuge"));
-let todo = new todoEntry("make todo");
-mainList.addEntry(todo);
 
 
